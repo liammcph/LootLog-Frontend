61f243b3f2c3e466524d88262db1334429fd79b5
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-05-10 16:31:41 -0700

    updated authService

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,32 @@ import SignUpForm from "./components/SignUpForm/SignUpForm";
 import SignInForm from "./components/SignInForm/SignInForm";
 import * as authService from "./services/authService";
 
+import Navbar from "./Navbar/Navbar";
+import Home from "./components/Home/Home";
+import LootLog from "./components/LootLog/LootLog";
+
+const App = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const user = JSON.parse(atob(token.split(".")[1])).user;
+      setUser(user);
+    }
+  }, [setUser]);
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/lootlog" element={<LootLog />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
