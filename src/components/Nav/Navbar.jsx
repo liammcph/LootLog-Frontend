7d62383c4748617acc1e62d1905f44d2312cf84f
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-05-11 21:11:53 -0700

    consolidated nav folders

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router';
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { Link } from "react-router";
 
-import './NavBar.css';
+import "./NavBar.css";
 
 const Navbar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = async () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/income">Income</Link>
-      <Link to="/expense">Expense</Link>
-      <Link to="/goal">Goal</Link>
-      <Link to="/sign-up">Sign-Up</Link>
-      <Link to="/sign-in">Sign-In</Link>
+      <ul>
+        {user ? (
+          <>
+            <li>
+              <Link to="/">Welcome Back {user.username}</Link>
+              <Link to="/income">Income</Link>
+              <Link to="/expense">Expense</Link>
+              <Link to="/goal">Goal</Link>
+              <Link to="/" onClick={handleSignOut}>
+                Sign Out
+              </Link>
+            </li>
+            {user && (
+              <li>
+                <div title={user.username}>
+                  {user.username.charAt(0).toUpperCase()}
+                </div>
+              </li>
+            )}
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/sign-up">Sign Up</Link>
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+          </>
+        )}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
